7086b59dde0a2eaa46b3465aa65ae07789e3a79c
User <User@DESKTOP-2RC2UP5>
2019-09-24 19:29:06 +0700

    first commit

---
+++ src/latihan1/Latihan1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+
+/**
+ * NAMA:REZA PRATAMA
+ * KELAS:PBO2
+ * NIM:10118055
+ * Deskripsi:program ini berisi program untuk menampilkan
+ * jenis jenis data bilangan bulat
+ */
+public class Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR"+"\n"+"---------------");
+        System.out.println("NAMA\t\t:REZA PRATAMA ");
+        System.out.println("ALAMAT\t\t:JALAN HARUM MEKAR ");
+        System.out.println("TEMPAT LAHIR\t:TANJUNG PANDAN");
+        System.out.println("TANGGAL LAHIR\t:15 MARET 2000");
+        System.out.println("---------------");
+        System.out.println("HOBI\t\t:LARI");
+        System.out.println("MAKES\t\t:SATE");
+        System.out.println("MIKES\t\t:JUS ALPUKAT");
+        
+        
+    }
+    
+}
